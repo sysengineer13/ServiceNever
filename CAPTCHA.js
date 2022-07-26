a3cf055294e6cf5e37fdf6c868c78e8e78f77f28
sysengineer <krislynsmith@outlook.com>
2022-07-26 11:24:50 -0400

    Added Where's waldo captcha

---
+++ CAPTCHA.js
@@ -0,0 +1,30 @@
+// set #image=https://whatever;content=whatever
+const hash = window.location.hash;
+const dict = hash
+  .substr(1)
+  .split(";")
+  .reduce((mem, cur) => {
+    const [key, val] = cur.split("=");
+    mem[key] = val;
+    return mem;
+  }, {});
+
+const updateImage = (image, content = "Waldo") => {
+  const contentElem = document.getElementById("content");
+  const grid = document.querySelector(".waldogrid");
+
+  contentElem.textContent = content;
+
+  grid.style.backgroundImage = `url(${image})`;
+};
+
+if (dict.image && dict.content) {
+  updateImage(dict.image, dict.content);
+}
+
+document.body.addEventListener("click", (e) => {
+  const target = e.target;
+  if (target.classList.contains("square")) {
+    target.classList.toggle("selected");
+  }
+});
